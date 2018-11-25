35a55eb56075543a8fc8fb451be1fb709c7d5534
yapjinai <jinai.jyap@gmail.com>
2018-11-25 18:39:31 -0500

    added seeds

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,6 @@
 class SongsController < ApplicationController
+  def index
+    @Songs = Song.all
+    render json: @Songs
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,131 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Song.delete_all
+
+baseurl = 'http://localhost:3001'
+
+s1 = Song.create(
+  title: 'Smile',
+  artist: 'Lily Allen',
+  bpm: 96,
+  url: "#{baseurl}/Smile.mp3"
+)
+s2 = Song.create(
+  title: 'Born To Die',
+  artist: 'Lana Del Rey',
+  bpm: 86,
+  url: "#{baseurl}/BornToDie.mp3"
+)
+s3 = Song.create(
+  title: 'Latch',
+  artist: 'Disclosure',
+  bpm: 122,
+  url: "#{baseurl}/Latch.mp3"
+)
+s4 = Song.create(
+  title: 'Super Natural',
+  artist: 'Carly Rae Jepsen',
+  bpm: 128,
+  url: "#{baseurl}/SuperNatural.mp3"
+)
+s5 = Song.create(
+  title: 'Get Got',
+  artist: 'Death Grips',
+  bpm: 87,
+  url: "#{baseurl}/GetGot.mp3"
+)
+s6 = Song.create(
+  title: 'Goooo',
+  artist: 'TNGHT',
+  bpm: 143,
+  url: "#{baseurl}/Goooo.mp3"
+)
+s7 = Song.create(
+  title: 'All Your Love',
+  artist: 'Hudson Mohawke',
+  bpm: 85,
+  url: "#{baseurl}/AllYourLove.mp3"
+)
+s8 = Song.create(
+  title: 'Stay Away',
+  artist: 'Charli XCX',
+  bpm: 183,
+  url: "#{baseurl}/StayAway.mp3"
+)
+s9 = Song.create(
+  title: 'Trophy',
+  artist: 'Charli XCX',
+  bpm: 127,
+  url: "#{baseurl}/Trophy.mp3"
+)
+s10 = Song.create(
+  title: 'Paradise',
+  artist: 'Charli XCX',
+  bpm: 168,
+  url: "#{baseurl}/Paradise.mp3"
+)
+s11 = Song.create(
+  title: 'GT40',
+  artist: 'Lindsay Lowend',
+  bpm: 165,
+  url: "#{baseurl}/GT40.mp3"
+)
+
+s12 = Song.create(
+  title: 'Wavvy',
+  artist: 'Mykki Blanco',
+  bpm: 95,
+  url: "#{baseurl}/Wavvy.mp3"
+)
+s13 = Song.create(
+  title: 'Kiss',
+  artist: 'GFOTY',
+  bpm: 130,
+  url: "#{baseurl}/Kiss.wav"
+)
+s14 = Song.create(
+  title: 'Dead Island',
+  artist: 'Kitty',
+  bpm: 117,
+  url: "#{baseurl}/DeadIsland.mp3"
+)
+s15 = Song.create(
+  title: 'VYZEE',
+  artist: 'SOPHIE',
+  bpm: 130,
+  url: "#{baseurl}/VYZEE.mp3"
+)
+s16 = Song.create(
+  title: 'Thunder Bay',
+  artist: 'Hudson Mohawke',
+  bpm: 136,
+  url: "#{baseurl}/ThunderBay.mp3"
+)
+s17 = Song.create(
+  title: 'Toxic',
+  artist: 'Britney Spears',
+  bpm: 143,
+  url: "#{baseurl}/Toxic.mp3"
+)
+s18 = Song.create(
+  title: 'Georgia',
+  artist: 'Charlie White and Boom Bip',
+  bpm: 130,
+  url: "#{baseurl}/Georgia.mp3"
+)
+s19 = Song.create(
+  title: 'Money On A Gold Plate',
+  artist: 'A. G. Cook',
+  bpm: 160,
+  url: "#{baseurl}/MoneyOnAGoldPlate.mp3"
+)
+s20 = Song.create(
+  title: 'Lover',
+  artist: 'GFOTY',
+  bpm: 170,
+  url: "#{baseurl}/Lover.wav"
+)
+s21= Song.create(
+  title: 'Inspector Norse',
+  artist: 'Todd Terje',
+  bpm: 120,
+  url: "#{baseurl}/InspectorNorse.mp3"
+)
