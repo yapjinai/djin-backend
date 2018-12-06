f26d1275bb55b56f3b9e4320fa4edee7644ef70b
yapjinai <jinai.jyap@gmail.com>
2018-12-06 10:59:37 -0500

    seed with other data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2476,3 +2476,358 @@ Song.create(
 #   url: "#{baseurl}/MarbleSoda.mp3",
 #   tags: ""
 # )
+
+########################
+
+otherbaseurl = 'https://s3.us-east-2.amazonaws.com/djin/others'
+
+Song.create(
+  title: "Believer",
+  artist: "Imagine Dragons",
+  bpm: 124.949,
+  url: "#{otherbaseurl}/Imagine+Dragons+-+Believer.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Under Pressure",
+  artist: "Queen",
+  bpm: 113.808,
+  url: "#{otherbaseurl}/Queen+-+Under+Pressure+(Lyrics).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "The Fox (What Does The Fox Say?)",
+  artist: "Ylvis",
+  bpm: 128.008,
+  url: "#{otherbaseurl}/The+Fox+(What+Does+The+Fox+Say).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "I'm Too Sexy",
+  artist: "Right Said Fred",
+  bpm: 122.113,
+  url: "#{otherbaseurl}/I'm+Too+Sexy.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "미쳐 (Crazy)",
+  artist: "4MINUTE",
+  bpm: 176.023,
+  url: "#{otherbaseurl}/4MINUTE+-+%E1%84%86%E1%85%B5%E1%84%8E%E1%85%A7(Crazy).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Whole New World",
+  artist: "Aladdin Soundtrack",
+  bpm: 124.805,
+  url: "#{otherbaseurl}/A+Whole+New+World+-+Aladdin+(lyrics).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Never Gonna Give You Up",
+  artist: "Rick Astley",
+  bpm: 113.33,
+  url: "#{otherbaseurl}/Never+Gonna+Give+You+Up+audio.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "It's So Cold in the D",
+  artist: "T BABY / Youtube poop",
+  bpm: 80.047,
+  url: "#{otherbaseurl}/It's+So+Cold+in+the+D.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Dog of Wisdom",
+  artist: "Joe / Youtube poop",
+  bpm: 100,
+  url: "#{otherbaseurl}/Dog+of+Wisdom.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Awoo",
+  artist: "Lim Kim (김예림)",
+  bpm: 149.917,
+  url: "#{otherbaseurl}/Awoo.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "I Feel Love",
+  artist: "Donna Summer",
+  bpm: 128.129,
+  url: "#{otherbaseurl}/Donna+Summer+-+I+feel+love.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "And She Was",
+  artist: "Talking Heads",
+  bpm: 126.477,
+  url: "#{otherbaseurl}/And+She+Was+(Remastered).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Holdin On",
+  artist: "Flume",
+  bpm: 90.053,
+  url: "#{otherbaseurl}/Flume+-+Holdin+On.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Come Together",
+  artist: "The Beatles",
+  bpm: 165.007,
+  url: "#{otherbaseurl}/The+Beatles+-+Come+Together.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Rinse & Repeat",
+  artist: "Riton ft. Kah-Lo",
+  bpm: 121.994,
+  url: "#{otherbaseurl}/Riton+-+Rinse+%26+Repeat+(Official+Video)+ft.+Kah-Lo.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Pursuit Of Happiness",
+  artist: "Kid Cudi ft. MGMT",
+  bpm: 115.293,
+  url: "#{otherbaseurl}/Kid+CuDi+Pursuit+Of+Happiness.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "(Who?) Keeps Changing Your Mind (Daniel Bovie & Roy Rox Dub Mix)",
+  artist: "South Street Player",
+  bpm: 126.984,
+  url: "#{otherbaseurl}/South+Street+Player+-+(Who)+Keeps+Changing+Your+Mind+(Daniel+Bovie+%26+Roy+Rox+Dub+Mix).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Heaven Only Knows",
+  artist: "Bob Moses",
+  bpm: 106.993,
+  url: "#{otherbaseurl}/Bob+Moses+-+Heaven+Only+Knows+(Official+Audio).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Papa's Got a Brand New Bag",
+  artist: "James Brown",
+  bpm: 129.434,
+  url: "#{otherbaseurl}/Papa's+Got+a+Brand+New+Bag.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "It's Your Thing",
+  artist: "The Isley Brothers",
+  bpm: 185.273,
+  url: "#{otherbaseurl}/The+Isley+Brothers+-+It's+Your+Thing+(Audio).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Friday",
+  artist: "Rebecca Black",
+  bpm: 112.004,
+  url: "#{otherbaseurl}/Friday+-+Rebecca+Black+-+Official+Music+Video.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "She Moves She",
+  artist: "Four Tet",
+  bpm: 114.033,
+  url: "#{otherbaseurl}/She+Moves+She.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Road of Resistance",
+  artist: "BABYMETAL",
+  bpm: 204.927,
+  url: "#{otherbaseurl}/BABYMETAL+-+Road+of+Resistance+(UNOFFICIAL)+short+ver..mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Girl Blunt",
+  artist: "Leikeli47",
+  bpm: 93.953,
+  url: "#{otherbaseurl}/Leikeli47+-+Girl+Blunt+(Audio).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "In My Feelings",
+  artist: "Drake",
+  bpm: 91.03,
+  url: "#{otherbaseurl}/Drake+-+In+My+Feelings+(Lyrics%2C+Audio)+Kiki+Do+you+love+me.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "London Tipton",
+  artist: "Juice Wrld",
+  bpm: 83,
+  url: "#{otherbaseurl}/Juice+Wrld+-+London+Tipton+(Official+Audio).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "like a farmer",
+  artist: "lil tracy (prod. gren8)",
+  bpm: 74,
+  url: "#{otherbaseurl}/tracy+-+like+a+farmer+(prod.+gren8).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "壊れかけのRadio",
+  artist: "Hideaki Tokunaga (德永英明)",
+  bpm: 76.621,
+  url: "#{otherbaseurl}/%E5%BE%B7%E6%B0%B8%E8%8B%B1%E6%98%8E+-+%E5%A3%8A%E3%82%8C%E3%81%8B%E3%81%91%E3%81%AERadio.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Strut",
+  artist: "Cheetah Girls",
+  bpm: 120.027,
+  url: "#{otherbaseurl}/Cheetah+Girls+2+-+Strut.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "She Thinks My Tractor's Sexy",
+  artist: "Kenny Chesney",
+  bpm: 122.07,
+  url: "#{otherbaseurl}/Kenny+Chesney+-+She+Thinks+My+Tractor's+Sexy+(2-Channel+Stereo+Mix).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Jet Blue Jet",
+  artist: "Major Lazer",
+  bpm: 127.96,
+  url: "#{otherbaseurl}/Jet+Blue+Jet.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Thank u, next",
+  artist: "Donald Trump / Youtube poop",
+  bpm: 106.96,
+  url: "#{otherbaseurl}/Ariana+Grande+-+Thank+u%2C+next+(cover+by+Donald+Trump+).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "c r o i s s a n t  d r e a m s (ｃｒｏｉｓｓａｎｔ ｄｒｅａｍｓ)",
+  artist: "Cyranek / Youtube poop",
+  bpm: 130,
+  url: "#{otherbaseurl}/%EF%BD%83%EF%BD%92%EF%BD%8F%EF%BD%89%EF%BD%93%EF%BD%93%EF%BD%81%EF%BD%8E%EF%BD%94+%EF%BD%84%EF%BD%92%EF%BD%85%EF%BD%81%EF%BD%8D%EF%BD%93.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "What Is Love",
+  artist: "Haddaway",
+  bpm: 123.864,
+  url: "#{otherbaseurl}/Haddaway+-+What+Is+Love+%5BOfficial%5D.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "My Heart Will Go On (Recorder version)",
+  artist: "Matt Mulholland / Youtube poop",
+  bpm: 98.347,
+  url: "#{otherbaseurl}/My+Heart+Will+Go+On+-+Recorder+By+Candlelight+by+Matt+Mulholland.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Africa",
+  artist: "Toto",
+  bpm: 92.717,
+  url: "#{otherbaseurl}/Toto+-+Africa+(Video).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Shiranami Top Water ( 白波トップウォーター )",
+  artist: "Sakanaction",
+  bpm: 125.967,
+  url: "#{otherbaseurl}/Sakanaction+-+Shiranami+Top+Water+(+%E7%99%BD%E6%B3%A2%E3%83%88%E3%83%83%E3%83%95%E3%82%9A%E3%82%A6%E3%82%A9%E3%83%BC%E3%82%BF%E3%83%BC+).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Motherlover",
+  artist: "The Lonely Island (ft. Justin Timberlake)",
+  bpm: 131.209,
+  url: "#{otherbaseurl}/Motherlover+(feat.+Justin+Timberlake).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "TINTS",
+  artist: "Anderson Paak (ft. Kendrick Lamar)",
+  bpm: 109.076,
+  url: "#{otherbaseurl}/Anderson+.Paak+-+TINTS+(feat.+Kendrick+Lamar)+(Official+Video).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Highway Tune",
+  artist: "Greta Van Fleet",
+  bpm: 123.993,
+  url: "#{otherbaseurl}/Greta+Van+Fleet+-+Highway+Tune.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Gold",
+  artist: "Chet Faker",
+  bpm: 139.975,
+  url: "#{otherbaseurl}/Chet+Faker+-+Gold+(Official+Music+Video).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Lotus Flower",
+  artist: "Radiohead",
+  bpm: 109.076,
+  url: "#{otherbaseurl}/Radiohead+-+Lotus+Flower.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Home",
+  artist: "LCD Soundsystem",
+  bpm: 124.013,
+  url: "#{otherbaseurl}/LCD+Soundsystem+-+Home.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Bring Sally Up",
+  artist: "Moby Flower",
+  bpm: 81,
+  url: "#{otherbaseurl}/Moby+Flower+-+Bring+Sally+Up+(High+Quality).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Kill Us All",
+  artist: "Twista",
+  bpm: 126.151,
+  url: "#{otherbaseurl}/Twista+-+Kill+Us+All.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Paso",
+  artist: "Sak Noel",
+  bpm: 128.012,
+  url: "#{otherbaseurl}/Sak+Noel+-+Paso+Lyrics.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "F-F-F-Falling",
+  artist: "The Rasmus",
+  bpm: 133.999,
+  url: "#{otherbaseurl}/The+Rasmus+-+F-F-F-Falling+(Official+Video).mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Tunak Tunak Tun",
+  artist: "Daler Mehndi",
+  bpm: 150.008,
+  url: "#{otherbaseurl}/Daler+Mehndi+-+Tunak+Tunak+Tun+Video.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Say It Ain't So",
+  artist: "Weezer",
+  bpm: 76.098,
+  url: "#{otherbaseurl}/Weezer+-+Say+It+Ain't+So.mp3",
+  tags: "flatiron"
+)
+Song.create(
+  title: "Who Let the Dogs Out?",
+  artist: "Baha Men",
+  bpm: 129.221,
+  url: "#{otherbaseurl}/Baha+Men+-+Who+Let+The+Dogs+Out+Lyrics.mp3",
+  tags: "flatiron"
+)
